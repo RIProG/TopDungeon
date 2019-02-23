5afcdd0d29a796ce4cd18e4e03c022ab9c3881e6
RIProG <riprog@hotmail.com>
2019-02-23 16:59:08 +0100

    complete

---
+++ TopDungeon/Assets/Scripts/CharacterMenu.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMenu : MonoBehaviour
+{
+    // Text fields
+    public Text levelText, hitPointText, pesosText, upgradeCostText, xpText;
+
+    // Logic
+    private int currentCharacterSelection = 0;
+    public Image characterSelectionSprite;
+    public Image weaponSprite;
+    public RectTransform xpBar;
+
+    // Character Selection
+    public void OnArrowClick(bool right)
+    {
+        if (right)
+        {
+            currentCharacterSelection++;
+
+            // If we went too far away in characterArray
+            if (currentCharacterSelection == GameManager.instance.playerSprites.Count)
+                currentCharacterSelection = 0;
+
+            OnSelectionChanged();
+        }
+        else
+        {
+            currentCharacterSelection--;
+            if (currentCharacterSelection < 0)
+                currentCharacterSelection = GameManager.instance.playerSprites.Count - 1;
+
+            OnSelectionChanged();
+        }
+
+    }
+    private void OnSelectionChanged()
+    {
+        characterSelectionSprite.sprite = GameManager.instance.playerSprites[currentCharacterSelection];
+        GameManager.instance.player.SwapSprite(currentCharacterSelection);
+    }
+
+    // Weapon Upgrade
+    public void OnUpgradeClick()
+    {
+        if (GameManager.instance.TryUpgradeWeapon())
+            UpdateMenu();
+    }
+
+    // Update the character information
+    public void UpdateMenu()
+    {
+        // Weapon
+        weaponSprite.sprite = GameManager.instance.weaponSprites[GameManager.instance.weapon.weaponLevel];
+        if (GameManager.instance.weapon.weaponLevel == GameManager.instance.weaponPrices.Count)
+            upgradeCostText.text = "MAX";
+        else
+            upgradeCostText.text = GameManager.instance.weaponPrices[GameManager.instance.weapon.weaponLevel].ToString();
+
+
+        // Meta
+        levelText.text = GameManager.instance.GetCurrentLevel().ToString();
+        hitPointText.text = GameManager.instance.player.hitPoint.ToString();
+        pesosText.text = GameManager.instance.pesos.ToString();
+
+        // Xp Bar
+        int currentLevel = GameManager.instance.GetCurrentLevel();
+        if (currentLevel == GameManager.instance.xpTable.Count)
+        {
+            xpText.text = GameManager.instance.experience.ToString() + " total experience points"; // Display total xp
+            xpBar.localScale = Vector3.one;
+        }
+        else
+        {
+            int previousLevelXp = GameManager.instance.GetXpToLevel(currentLevel - 1);
+            int currentLevelXp = GameManager.instance.GetXpToLevel(currentLevel);
+
+            int difference = currentLevelXp - previousLevelXp;
+            int currentXpIntoLevel = GameManager.instance.experience - previousLevelXp;
+
+            float completionRatio = (float)currentXpIntoLevel / (float)difference;
+            xpBar.localScale = new Vector3(completionRatio, 1, 1);
+            xpText.text = currentXpIntoLevel.ToString() + " / " + difference;
+        }
+        
+
+    }
+}
